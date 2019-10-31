27517a4a17bce5c360ef6f8f1f147aee6e447ff4
mx623303468 <623303468@qq.com>
2019-10-31 17:34:16 +0800

    1. 安装依赖，并引入 2. 初始化路由 3. 初始化Vuex状态 4. 引入loading-btn.css，loading.css

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,36 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import ChatDashboard from '../views/ChatDashboard.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: '/',
+    name: 'login',
+    component: Login
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/chat',
+    name: 'chat',
+    component: ChatDashboard
   }
-];
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
 
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
+const state = {
+  loading: false,
+  sending: false,
+  error: null,
+  user: [],
+  reconnect: false,
+  activeRoom: null,
+  rooms: [],
+  users: [],
+  messages: [],
+  userTyping: null
+}
+
+const getters = {
+  hasError: state => (state.error ? true : false)
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  mutations,
+  actions,
+  getters,
+  modules: {},
+  plugins: [vuexLocal.plugin],
+  strict: debug
 })
